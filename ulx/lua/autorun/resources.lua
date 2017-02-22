2f88476ec59b2377ef2669e4f874f1d1d8abe45d
zacklogan <bigred9921@mchsi.com>
2017-02-22 09:39:17 -0600

    Create resources.lua

---
+++ ulx/lua/autorun/resources.lua
@@ -0,0 +1,109 @@
+resource.AddWorkshop( '489264904' ) -- Vinyl Glowstick and Rings (Props) - Pony
+resource.AddWorkshop( '207948202' ) -- Simple ThirdPerson - Sliders & Fixes!
+resource.AddWorkshop( '770990581' ) -- Simple Third Person - Keybind Toggle
+resource.AddWorkshop( '493796394' ) -- Cartoony Pony Props
+resource.AddWorkshop( '847556572' ) -- CoD Nazi Zombies Playermodels
+resource.AddWorkshop( '468046989' ) -- Custom Pony Player Models
+resource.AddWorkshop( '124648402' ) -- Orbital Friendship Cannon
+resource.AddWorkshop( '104691717' ) -- PAC3
+resource.AddWorkshop( '205268453' ) -- Pony Player Models
+resource.AddWorkshop( '696374067' ) -- Drugs Mod [Original Version]
+resource.AddWorkshop( '106958912' ) -- Gemstone Money Printers (Requires DarkRP 2.6.x)
+resource.AddWorkshop( '293674909' ) -- Haus The Plague Doktor (Player_Ragdoll_NPCs)
+resource.AddWorkshop( '546392647' ) -- Media Player
+resource.AddWorkshop( '673698301' ) -- Vape SWEP
+resource.AddWorkshop( '297646940' ) -- Black Ops C4 (Sandbox)
+resource.AddWorkshop( '128089118' ) -- M9K Assault Rifles
+resource.AddWorkshop( '128091208' ) -- M9K Heavy Weapons
+resource.AddWorkshop( '639539553' ) -- M9K Pistols+
+resource.AddWorkshop( '749554285' ) -- M9K Rifles+
+resource.AddWorkshop( '834520203' ) -- M9K Shotguns+
+resource.AddWorkshop( '128093075' ) -- M9K Small Arms pack
+resource.AddWorkshop( '144982052' ) -- M9K Specialties
+resource.AddWorkshop( '787352178' ) -- Overwatch - Hanzo[PM/RAG/NPC]
+resource.AddWorkshop( '327281224' ) -- Roleplay Props
+resource.AddWorkshop( '415143062' ) -- TFA Base [ Reduxed ]
+resource.AddWorkshop( '712848264' ) -- TFA Counter-Strike: Online / Counter-Strike Nexon: Zombie SWEPs
+resource.AddWorkshop( '807290390' ) -- Vindictus - Arisha [Playermodel/NPC]
+resource.AddWorkshop( '807268979' ) -- Vindictus - Dwyn [Playermodel/NPC]
+resource.AddWorkshop( '807304695' ) -- Vindictus - Kai [Playermodel/NPC]
+resource.AddWorkshop( '807277888' ) -- Vindictus - Vella  [Playermodel/NPC]
+resource.AddWorkshop( '807295642' ) -- Vindictus Viewhands
+resource.AddWorkshop( '795694167' ) -- Widowmaker [PM/RAG/VOX]
+resource.AddWorkshop( '832086722' ) -- [TFA] AVA Small Arms Pack
+resource.AddWorkshop( '848490709' ) -- [TFA] Killing Floor 2 Melees [ Parrying, Bashing, Directional Attacks, Combos and More ]
+resource.AddWorkshop( '821713377' ) -- [TFA] Sombra's Machine Pistol
+resource.AddWorkshop( '198236665' ) -- ASDF Movie SWEP Pack
+resource.AddWorkshop( '810792923' ) -- Big Smoke Playermodel
+resource.AddWorkshop( '848823438' ) -- Black Ops Sweps
+resource.AddWorkshop( '753707158' ) -- Death Machine
+resource.AddWorkshop( '585793005' ) -- Edible Food Mod
+resource.AddWorkshop( '738815378' ) -- Genji's Katana SWEP for TTT
+resource.AddWorkshop( '843917237' ) -- GET YOUR FREE TVS
+resource.AddWorkshop( '756601186' ) -- Grim Reaper
+resource.AddWorkshop( '832871822' ) -- Jet Gun
+resource.AddWorkshop( '758646160' ) -- Mustang and Sally
+resource.AddWorkshop( '104486597' ) -- NPC Tools
+resource.AddWorkshop( '706860888' ) -- Overwatch - Genji Playermodel/Ragdoll
+resource.AddWorkshop( '726069482' ) -- OW- Reaper Playermodel
+resource.AddWorkshop( '770711426' ) -- Soldier 76 Playermodel
+resource.AddWorkshop( '840285366' ) -- V-R11
+resource.AddWorkshop( '131759821' ) -- VJ Base
+resource.AddWorkshop( '763093604' ) -- Winters Howl
+resource.AddWorkshop( '836462420' ) -- World at War Sweps
+resource.AddWorkshop( '798965626' ) -- Young Genji/Sparrow Playermodel and NPCs
+resource.AddWorkshop( '406018213' ) -- [DarkRP] Keep pocket contents when disconnecting
+resource.AddWorkshop( '790671732' ) -- [TFA] Reaper Hellfire Shotguns
+resource.AddWorkshop( '258063586' ) -- RP Retribution
+resource.AddWorkshop( '761321077' ) -- Black Ops 3 Perk a Cola Machine Models + Pack a Punch
+resource.AddWorkshop( '163398108' ) -- CoD BO2 Zombies Perk a cola Machine Models
+resource.AddWorkshop( '158061274' ) -- Angry Hobo SWEP
+resource.AddWorkshop( '649016805' ) -- Simple DarkRP HUD v1.0
+resource.AddWorkshop( '673317324' ) -- DSit - Sit anywhere
+resource.AddWorkshop( '675138912' ) -- nZombies - A Nazi Zombies Gamemode [Content Pack]
+resource.AddWorkshop( '777195612' ) -- nZombies Content Pack [Part 2]
+resource.AddWorkshop( '839003116' ) -- Thunder Gun
+resource.AddWorkshop( '720664530' ) -- R-Base - Easy-to-use & Efficient Nextbot Base
+resource.AddWorkshop( '726197311' ) -- [R-Base] Black Ops 1: Zombies Nextbots
+resource.AddWorkshop( '754274454' ) -- [R-Base] Black Ops 2: Zombies Nextbots
+resource.AddWorkshop( '109643223' ) -- 3D2D Textscreens
+resource.AddWorkshop( '628672890' ) -- Casual Girl (Playermodel)
+resource.AddWorkshop( '160250458' ) -- Wiremod
+resource.AddWorkshop( '628356025' ) -- Roleplay / Machinima Camera
+resource.AddWorkshop( '745217456' ) -- Rp Evening City V3
+resource.AddWorkshop( '430555371' ) -- Nodegraph MegaPack
+resource.AddWorkshop( '107471183' ) -- Rp_Amsterville
+resource.AddWorkshop( '354565921' ) -- rp_eastcoast (v 1.5)
+resource.AddWorkshop( '386891553' ) -- Canterlot
+resource.AddWorkshop( '687154026' ) -- rp_eastcoast_v4
+resource.AddWorkshop( '179517028' ) -- Zombie/NPC Invasion
+resource.AddWorkshop( '128002457' ) -- rp_necro_forest_a1
+resource.AddWorkshop( '750933567' ) -- Announcement Manager
+resource.AddWorkshop( '858039263' ) -- DarkRP Advert Fix!
+resource.AddWorkshop( '817813063' ) -- [FG] Fallout Equestria - Security Armor (PPM)
+resource.AddWorkshop( '663083175' ) -- 1950s Pack II
+resource.AddWorkshop( '163221745' ) -- Food Mod
+resource.AddWorkshop( '229976936' ) -- Join, Connect, and Disconnect Messages
+resource.AddWorkshop( '439856500' ) -- Pointshop 2 Content
+resource.AddWorkshop( '649613347' ) -- Arp_TheCordon
+resource.AddWorkshop( '369842321' ) -- Pointshop 2 Assets
+resource.AddWorkshop( '370467723' ) -- Pointshop 2 Boosters DLC Assets
+resource.AddWorkshop( '187930698' ) -- Rp_HeadattackCity_V1
+resource.AddWorkshop( '104694154' ) -- Mane 6 Ponies
+resource.AddWorkshop( '256787177' ) -- PonyPistol (mlp) - Request
+resource.AddWorkshop( '471982639' ) -- RP_Outskirts
+resource.AddWorkshop( '131302441' ) -- RP_Littletown
+resource.AddWorkshop( '669642096' ) -- Drones Rewrite
+resource.AddWorkshop( '392979531' ) -- Ponies's wolf costume
+resource.AddWorkshop( '557997149' ) -- PPM Socks
+resource.AddWorkshop( '420970650' ) -- Darken217's SciFi Weapons
+resource.AddWorkshop( '762284291' ) -- Fading Doors - Original
+resource.AddWorkshop( '108424005' ) -- Keypad Tool and Cracker with Wire Support
+resource.AddWorkshop( '290282950' ) -- Riot Shield
+resource.AddWorkshop( '158882496' ) -- Police Baton SWEP (spring)
+resource.AddWorkshop( '720768756' ) -- Arcade Machine
+resource.AddWorkshop( '802395670' ) -- Doviculus, Ophelia guitars
+resource.AddWorkshop( '862034136' ) -- Nintendo 3ds XL
+resource.AddWorkshop( '853492727' ) -- Nintendo Switch (new version)
+resource.AddWorkshop( '260896911' ) -- rp_uptown_v4
+resource.AddWorkshop( '176850029' ) -- rp_riveley
